f75e59fdbc2328d4e73537fb6944079385345bdd
Chase <st10433939@vcconnect.edu.za>
2026-04-22 22:44:08 +0200

    Added xUnit test

---
+++ xUnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using _10433939_PROG7311_Part2.Data;
+using _10433939_PROG7311_Part2.Models;
+using System.Text;
+
+namespace xUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1_AddClient_Successful()
+        {
+            //Create new
+            var initialCount = ClientData.GetAllClients().Count();
+
+            var newClient = new Client
+            {
+                name = "Ben Sharp",
+                region = "South Africa"
+            };
+            //Action
+            ClientData.AddClient(newClient);
+
+            //New count
+            var newCount = ClientData.GetAllClients().Count();
+            Assert.Equal(initialCount + 1, newCount);
+
+            Assert.True(newClient.clientId > 0, "Client should have assigned Id.");
+
+            //Verify retrieval
+            var retrievedClient = ClientData.GetClientById(newClient.clientId);
+            Assert.NotNull(retrievedClient);
+            Assert.Equal("Ben Sharp", retrievedClient.name);
+        }
+
+    }
+}
